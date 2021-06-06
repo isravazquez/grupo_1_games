11b700c2122dbd3acc7332761c435322845b1f3a
isravazquez <israfullshot@gmail.com>
2021-06-06 18:56:53 -0500

    Cree app.js con estructura de servidor express, inicialice npm, instalé express, añadí home.html y home-styles.css, el archivo contiene el header en desarrollo y el footer en desarrollo

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use( express.static(path.resolve(__dirname, './public')) )
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en marcha en el puerto 3000");
+});
