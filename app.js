a3ee57aadbb0c3dd551a898bd9faaa53176256f8
TheStealer <k_hintze_m@hotmail.com>
2021-06-08 18:28:20 -0500

    .

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use( express.static(path.resolve(__dirname, './public')) )
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
 
 app.listen(3000, () => {
-    console.log("Servidor en marcha en el puerto 3000");
+  console.log("Servidor en marcha en el puerto 3000");
 });
