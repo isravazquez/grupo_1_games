8778e458ca21f8aa23ba42ff7739d1cbacbff615
isravazquez <israfullshot@gmail.com>
2021-06-08 23:24:05 -0500

    Añadí el header y footer para movil al home.html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use( express.static(path.resolve(__dirname, './public')) )
+app.use( express.static(path.resolve(__dirname, './Public')) )
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
