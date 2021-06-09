6af2a2411ab207c2709efaedd1056fcf75168403
TheStealer <k_hintze_m@hotmail.com>
2021-06-08 22:19:47 -0500

    pequeñas modificaciones para hacer links a otras paginas

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
-
+console.log(path.resolve(__dirname, "./public"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 app.get("/login", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+app.get("/signup", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/signup.html"));
 });
 
 app.listen(3000, () => {
