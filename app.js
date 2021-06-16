2f9a22d53aa1589cfb0aa5deafdfdeb90d70d04a
isravazquez <israfullshot@gmail.com>
2021-06-16 18:07:26 -0500

    Heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
-console.log(path.resolve(__dirname, "./public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
