2614396b3202cec29411b6bd2e5b31618b758cd2
isravazquez <jaimepedobear@gmail.com>
2021-06-25 19:23:14 -0500

    commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
---
+++ controllers/mainController.js
@@ -0,0 +1,7 @@
+const controller = {
+    home: (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./views/home.html"));
+      };
+    product: ;
+    cart: ;
+}
