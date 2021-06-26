c9bf9d86ada75104c60f07765484d1a3c2979a87
TheStealer <k_hintze_m@hotmail.com>
2021-06-25 20:00:04 -0500

    Routes

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const homeRoute = require("./routes/home");
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs')
-
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "./public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/home.html"));
-});
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/login.html"));
-});
-app.get("/signup", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/signup.html"));
-});
-app.get("/productDetail", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
-});
-app.get("/shoppingCart", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/shoppingCart.html"));
-});
+app.set("views", path.join(__dirname, "views"));
+app.use("/", homeRoute);
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./views/home.html"));
+// });
+// app.get("/login", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./views/login.html"));
+// });
+// app.get("/signup", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./views/signup.html"));
+// });
+// app.get("/productDetail", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+// });
+// app.get("/shoppingCart", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./views/shoppingCart.html"));
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,16 @@
-const controller = {
-    home: (req, res) => {
-        res.sendFile(path.resolve(__dirname, "./views/home.html"));
-      };
-    product: ;
-    cart: ;
-}
+const mainController = {
+  home: (req, res) => {
+    res.render("home");
+  },
+  product: (req, res) => {
+    res.render("productDetail");
+  },
+  cart: (req, res) => {
+    res.render("shoppingCart");
+  },
+  login: (req, res) => {
+    res.render("login");
+  },
+};
+
+module.exports = mainController;
