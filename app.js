73cfc580d33aa82e62784c57d3838d4f768283de
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-06-28 12:06:42 -0500

    se agregaron rutas y vonfiguracion para peticion post

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const homeRoute = require("./routes/home");
+const productsRoute = require("./routes/productsRoute");
+
 app.set("view engine", "ejs");
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.set("views", path.join(__dirname, "views"));
 app.use("/", homeRoute);
+app.use("/products", productsRoute);
 // app.get("/", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 // });
--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get("/", mainController.home);
 router.get("/login", mainController.login);
 router.get("/cart", mainController.cart);
-router.get("/product", mainController.product);
+
 
 module.exports = router;
