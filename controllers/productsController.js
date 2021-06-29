e24a55d1a98ec89a8f1b2cc12bb1322edd1e3696
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-06-29 11:57:19 -0500

    se agregaron operaciones CRUD de productos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -51,16 +51,16 @@ const productsController = {
     
 
       //mensaje de confirmacion
-      res.send('datos recibidos..').status(200);
+      // res.send('datos recibidos..').status(200);
       
       
       let productTmp = {
         id: parseInt(req.body.id),
         name: req.body.name,
         category: req.body.category,
-        price: "$"+req.body.price,
+        price: parseInt(req.body.price),
         discountRate: parseInt(req.body.discountRate),
-        discount: "$"+((req.body.price-(req.body.discountRate/100)*req.body.price)), //precio a pagar menos el descuento
+        discount: parseInt(((req.body.price-(req.body.discountRate/100)*req.body.price))), //precio a pagar menos el descuento
         stock: parseInt(req.body.stock),
         description: req.body.description,
         image: req.body.image,
@@ -80,6 +80,102 @@ const productsController = {
       //escritura de archivo
 
       fs.writeFileSync(path.resolve(__dirname,'products.json'),cadenaJsonE); 
+
+      res.redirect('createProduct');
+    },
+    listProducts: (req, res) => {
+      // apertura de archivo
+      let cadenaJsonA = fs.readFileSync(path.resolve(__dirname,'products.json'),'utf-8');
+      // conversion de objeto a cadena json
+      let listaProducts = JSON.parse(cadenaJsonA);
+      // retorno datos 
+      //envio de datos a vista 
+      res.render('listProducts', { products: listaProducts});
+    },
+    editProduct:(req, res)=>{
+      // apertura de archivo
+      let cadenaJsonA = fs.readFileSync(path.resolve(__dirname,'products.json'),'utf-8');
+      // conversion de objeto a cadena json
+      let listaProducts = JSON.parse(cadenaJsonA);
+
+
+      let productoEncontrado = listaProducts.find( (producto) => {
+        return producto.id == parseInt(req.params.id);
+      });
+
+      console.log(productoEncontrado.price);
+      if(productoEncontrado != null){
+        res.render('editProduct', { product: productoEncontrado});
+      }else{
+        res.send("producto no encontrado...");
+      }
+
+    },
+    updateProduct:(req, res)=>{
+    
+
+      // apertura de archivo
+      let cadenaJsonA = fs.readFileSync(path.resolve(__dirname,'products.json'),'utf-8');
+      // conversion de objeto a cadena json
+      let listaProducts = JSON.parse(cadenaJsonA);
+
+
+      let productoEncontrado = listaProducts.find( (producto) => {
+        return producto.id == parseInt(req.params.id);
+      });
+
+      if(productoEncontrado != null){
+
+        console.log(productoEncontrado);
+        
+        let fecha = new Date();
+      
+        //fecha 
+        const map = {
+          dd: fecha.getDate(),
+          mm:  fecha.getMonth()+1 <= 9 ? '0'+(fecha.getMonth()+1) : (fecha.getMonth()+1),     //podriamos arreglar que agregue un cero en el mes
+          yy: fecha.getFullYear().toString().slice(-2),
+          yyyy: fecha.getFullYear()
+        }
+
+        let productTmp = {
+          id: parseInt(req.body.id),
+          name: req.body.name,
+          category: req.body.category,
+          price: parseInt(req.body.price),
+          discountRate: parseInt(req.body.discountRate),
+          discount: parseInt(((req.body.price-(req.body.discountRate/100)*req.body.price))), //precio a pagar menos el descuento
+          stock: parseInt(req.body.stock),
+          description: req.body.description,
+          image: req.body.image,
+          features: req.body.features,
+          //extras
+          registrationDate: 'dd/mm/yy'.replace(/dd|mm|yy|yyy/gi, matched => map[matched]),
+          checkInTime: fecha.getHours()+":"+fecha.getMinutes()+" "+(fecha.getHours() >= 12 ? 'PM' : 'AM'),
+          userWhoRegistered: req.body.userWhoRegistered,
+        }; 
+
+        let productoModificado={};
+        productoModificado = Object.assign(productoModificado, productoEncontrado, productTmp);
+
+        let indice = listaProducts.findIndex( (producto) => {
+              return producto.id == parseInt(req.params.id);
+        });
+
+        listaProducts[indice] = productoModificado;
+
+
+        //conversion de objeto a cadena json
+        let cadenaJsonE = JSON.stringify(listaProducts,null, 2);
+        //escritura de archivo
+        fs.writeFileSync(path.resolve(__dirname,'products.json'),cadenaJsonE); 
+        
+        //redireccion a editar productos
+        res.render('listProducts');
+
+      }else{
+        res.send("producto no encontrado...");
+      }
 
     }
   };
