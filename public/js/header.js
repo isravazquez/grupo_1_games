85b037c5581d3ea97fb013128df46313f1c4aa87
TheStealer <k_hintze_m@hotmail.com>
2021-07-06 09:55:22 -0500

    minimos cmabios js

--- public/js/header.js
+++ public/js/header.js
@@ -1,15 +1,17 @@
 let searchIcon = document.getElementsByClassName("buscador-icon")[0];
 let searchBar = document.getElementById("search-bar");
-let searchBarStatus = searchBar.style.visibility;
+var searchBarStatus = searchBar.style.width;
 let navBarMenu = document.querySelector(".dropButton");
 let dropMenu = document.querySelector(".nav-bar-drop");
 searchIcon.addEventListener("click", () => {
-  if (searchBar.style.width == "0vw" || searchBar.style.width == "") {
-    searchBar.style.width = 40 + "vw";
+  if (searchBarStatus == "0vw" || searchBarStatus == "") {
+    searchBarStatus = 40 + "vw";
+    searchBar.style.width = searchBarStatus;
     searchBar.style.animation = "slide-in 1s";
   } else {
     searchBar.style.animation = "slide-out 1s";
-    searchBar.style.width = 0 + "vw";
+    searchBarStatus = 0 + "vw";
+    searchBar.style.width = searchBarStatus;
   }
 });
 
