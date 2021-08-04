5f89ed4db533c371708991a2806c08b89f8cebf5
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-08-03 20:00:26 -0500

    se agrego middleware a ruta de login para que usuarios logeados, no se vuelvan a loguear

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
+const guestMiddleware = require('../middlewares/guestMiddleware');
 
 router.get("/", mainController.home);
-router.get("/login", mainController.login);
+router.get("/login",guestMiddleware , mainController.login);
 router.get("/signup", mainController.signup);
 router.get("/cart", mainController.cart);
 
