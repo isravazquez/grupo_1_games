553d27166ce77bb05a98a64f10e29d83894b70a4
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-09-01 10:21:33 -0500

    metodos para modelos en base da detos

--- models/modelProducts.js
+++ models/modelProducts.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 const fs2 = require('fs').promises;
 const path = require('path');
 
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
 const modelProducts = {
     
-     
     aperturaDeArchivo: function () {
         //apertura de archivo
         const cadenaJsonA = fs.readFileSync(path.resolve(__dirname,'../data/products.json'),'utf-8');
@@ -20,6 +22,107 @@ const modelProducts = {
         //escritura de archivo
         fs.writeFileSync(path.resolve(__dirname,'../data/products.json'),cadenaJsonE); 
         
+    },
+    objetByDB: function (req) {
+       //objeto para fechas 
+       const fecha = new Date();
+
+       //para obtener la fecha con formato 
+       const map = {
+            dd: fecha.getDate(),
+            mm:  fecha.getMonth()+1 <= 9 ? '0'+(fecha.getMonth()+1) : (fecha.getMonth()+1),     //podriamos arreglar que agregue un cero en el mes
+            yy: fecha.getFullYear().toString(),
+            yyyy: fecha.getFullYear()
+       }
+
+  
+      
+       // console.log('yy-mm-dd'.replace(/yy|mm|dd|yyy/gi, matched => map[matched]));
+        const productoTmp = {
+                name: req.body.name,
+                categoryId: req.body.category,
+                price: parseInt(req.body.price),
+                discountRate: parseInt(req.body.discountRate),
+                discount: parseInt(((req.body.price-(req.body.discountRate/100)*req.body.price))), //precio a pagar menos el descuento
+                stock: parseInt(req.body.stock),
+                description: req.body.description,
+                // image: imagenes[0],     //para imagen principal 
+                // imagesSec: imagenes,    //para imagenes secundarias
+                features: req.body.features,
+                //extras fecha de registro, hora de registro y usuario que hio el registro 
+                registrationDatetime: 'yy-mm-dd'.replace(/yy|mm|dd|yyy/gi, matched => map[matched]),
+                // checkInTime: fecha.getHours()+":"+fecha.getMinutes()+" "+(fecha.getHours() >= 12 ? 'PM' : 'AM'),
+                userWhoRegistered: req.body.userWhoRegistered
+        };
+
+        return productoTmp;
+
+
+    },
+    objetByDBPut: async function (req) {
+       //objeto para fechas 
+       const fecha = new Date();
+
+       //para obtener la fecha con formato 
+       const map = {
+            dd: fecha.getDate(),
+            mm:  fecha.getMonth()+1 <= 9 ? '0'+(fecha.getMonth()+1) : (fecha.getMonth()+1),     //podriamos arreglar que agregue un cero en el mes
+            yy: fecha.getFullYear().toString(),
+            yyyy: fecha.getFullYear()
+       }
+
+        const categoriaE = await db.Category.findAll({
+            where: {
+                name: req.body.category.trim()
+            }
+        }).catch((error) => {
+          console.log('Error de: '+ error);
+        });
+
+        // console.log(categoriaE);
+        // console.log(categoriaE[0].id);
+        
+       // console.log('yy-mm-dd'.replace(/yy|mm|dd|yyy/gi, matched => map[matched]));
+        const productoTmp = {
+                name: req.body.name,
+                categoryId: categoriaE[0].id,
+                price: parseInt(req.body.price),
+                discountRate: parseInt(req.body.discountRate),
+                discount: parseInt(((req.body.price-(req.body.discountRate/100)*req.body.price))), //precio a pagar menos el descuento
+                stock: parseInt(req.body.stock),
+                description: req.body.description,
+                // image: imagenes[0],     //para imagen principal 
+                // imagesSec: imagenes,    //para imagenes secundarias
+                features: req.body.features,
+                //extras fecha de registro, hora de registro y usuario que hio el registro 
+                registrationDatetime: 'yy-mm-dd'.replace(/yy|mm|dd|yyy/gi, matched => map[matched]),
+                // checkInTime: fecha.getHours()+":"+fecha.getMinutes()+" "+(fecha.getHours() >= 12 ? 'PM' : 'AM'),
+                userWhoRegistered: req.body.userWhoRegistered
+        };
+
+        return productoTmp;
+
+
+    },
+    imagesByDb:function (req) {
+        //para validar se han cargado imagenes desde el formulario
+        const imagenes = [];
+        // req.files.imageProducto[0].filename
+        if(req.files){
+            //imagenes 
+            // console.log(req.files);
+            
+            req.files.forEach( imagen => {
+                // console.log(imagen);
+                imagenes.push(imagen.filename);
+            });
+
+        }else{
+            imagenes = [];
+        }
+
+        //console.log(imagenes);
+        return imagenes;
     },
     estructurarObjetoPOST: function (req) {
         
@@ -54,7 +157,7 @@ const modelProducts = {
         }
 
         //console.log(imagenes);
-    // ---------validacion de imagenes---------------
+        // ---------validacion de imagenes---------------
 
         
         //creacion de objeto temporal que almacena los datos entrantes de la vista crear producto
