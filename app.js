c82a46599f6101d189f617f0ec764a28f764fa5e
TheStealer <k_hintze_m@hotmail.com>
2021-10-07 19:12:03 -0500

    Agregué modulo de cors para base de datos con React

--- app.js
+++ app.js
@@ -4,24 +4,25 @@ const app = express();
 const homeRoute = require("./routes/home");
 const productsRoute = require("./routes/productsRoute");
 const usersRoute = require("./routes/userRoute");
+const cors = require("cors");
 
 const apiProductsRoute = require("./routes/api/apiProductsRoute");
 
 const methodOverride = require("method-override");
 const session = require("express-session");
-const usuarioLogueadoMiddleware = require('./middlewares/usuarioLogueadoMiddleware');
+const usuarioLogueadoMiddleware = require("./middlewares/usuarioLogueadoMiddleware");
 
 app.set("view engine", "ejs");
 
 // configurar los metodos post
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(session({ secret: "secreto",
-		resave : false,
-		saveUninitialized : false
-}));
-//middleware para verifcar session activa 
+app.use(
+  session({ secret: "secreto", resave: false, saveUninitialized: false })
+);
+//middleware para verifcar session activa
 app.use(usuarioLogueadoMiddleware);
+app.use(cors());
 
 // configurar los metodos delete y put con method-override
 app.use(methodOverride("_method"));
@@ -35,6 +36,6 @@ app.use("/users", usersRoute);
 //api
 app.use("/api/products", apiProductsRoute);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Servidor corriendo en el puerto 3000");
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Servidor corriendo en el puerto 3001");
 });
